cee7cfb6701318c5a003b211dbb3e29c144f9af2
Drew <drew.torgeson@gmail.com>
2016-04-10 21:55:40 -0600

    Navbar lights the active page

--- wramtas/public/js/controllers/mainController.js
+++ wramtas/public/js/controllers/mainController.js
@@ -1,3 +1,36 @@
 ﻿
-app.controller('mainController', function ($scope) {
+app.controller('mainController', function ($scope, $location) {
+    $scope.tabs = [
+        {
+            href: '/',
+            title: 'News'
+        },
+        {
+            href: '/about',
+            title: 'About'
+        },
+        {
+            href: '/affiliates',
+            title: 'Affiliates'
+        },
+        {
+            href: '/conference',
+            title: 'WRAMTA Conference'
+        },
+        {
+            href: '/spotlight',
+            title: 'Spotlight'
+        },
+        {
+            href: '/profile',
+            title: 'My WRAMTAS Profile'
+        },
+        {
+            href: '/elections',
+            title: 'Elections'
+        },];
+
+    $scope.isActive = function(currentPage) {
+        return currentPage === $location.path();
+    };
 });
