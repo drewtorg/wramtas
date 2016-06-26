dff03fca0906eddef6f5092a1ba21888541b6055
Drew Torgeson <drew.torgeson@gmail.com>
2016-06-26 16:53:11 -0600

    Create form for editing bios

--- wramtas/public/js/directives/bioDirective.js
+++ wramtas/public/js/directives/bioDirective.js
@@ -8,42 +8,42 @@ app.directive('bio', function(biosService) {
       onDelete: '&',
     },
     controller: ['$scope', function($scope) {
-      // $scope.bio.inEditMode = angular.isDefined($scope.bio.inEditMode) ? $scope.bio.inEditMode : false;
-      //
-      // $scope.toggleEditMode = function() {
-      //   $scope.bio.inEditMode = !$scope.bio.inEditMode;
-      // };
-      //
-      // $scope.editBio = function() {
-      //   $scope.tempBio = angular.copy($scope.bio);
-      //   $scope.toggleEditMode();
-      // };
-      //
-      // $scope.deleteBio = function() {
-      //   $scope.onDelete();
-      //   biosService.deleteBios($scope.type, $scope.bio);
-      // };
-      //
-      // $scope.saveBio = function() {
-      //   if (angular.isDefined($scope.tempBio))
-      //     $scope.bio.about = angular.copy($scope.tempBio.about);
-      //
-      //   biosService.saveBio($scope.type, $scope.bio).then(function(response) {
-      //     $scope.toggleEditMode();
-      //   });
-      // };
-      //
-      // $scope.undoBio = function() {
-      //   // this is the case when we are undoing after clicking edit
-      //   if (angular.isDefined($scope.tempBio))
-      //     $scope.tempBio.about = angular.copy($scope.bio.about);
-      //
-      //   // this is the case when we are undoing after clicing add
-      //   if (!angular.isDefined($scope.tempBio) || $scope.tempBio.about === '')
-      //     $scope.deleteBio();
-      //
-      //   $scope.toggleEditMode();
-      // };
+      $scope.bio.inEditMode = angular.isDefined($scope.bio.inEditMode) ? $scope.bio.inEditMode : false;
+
+      $scope.toggleEditMode = function() {
+        $scope.bio.inEditMode = !$scope.bio.inEditMode;
+      };
+
+      $scope.editBio = function() {
+        $scope.tempBio = angular.copy($scope.bio);
+        $scope.toggleEditMode();
+      };
+
+      $scope.deleteBio = function() {
+        $scope.onDelete();
+        biosService.deleteBio($scope.type, $scope.bio);
+      };
+
+      $scope.saveBio = function() {
+        if (angular.isDefined($scope.tempBio))
+          $scope.bio.about = angular.copy($scope.tempBio.about);
+
+        biosService.saveBio($scope.type, $scope.bio).then(function(response) {
+          $scope.toggleEditMode();
+        });
+      };
+
+      $scope.undoBio = function() {
+        // this is the case when we are undoing after clicking edit
+        if (angular.isDefined($scope.tempBio))
+          $scope.tempBio.about = angular.copy($scope.bio.about);
+
+        // this is the case when we are undoing after clicing add
+        if (!angular.isDefined($scope.tempBio) || $scope.tempBio.about === '')
+          $scope.deleteBio();
+
+        $scope.toggleEditMode();
+      };
     }]
   }
 });
