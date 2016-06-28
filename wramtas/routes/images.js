fa5a21cf95d083d795e0843ca697e50664a20f48
Drew Torgeson <drew.torgeson@gmail.com>
2016-06-27 22:03:17 -0600

    First round of flowjs implementation

---
+++ wramtas/routes/images.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var multer = require('multer');
+var upload = multer({
+  dest: './public/images/'
+}).single('userPhoto');
+
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+  upload(req, res, function(err) {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
+});
+
+router.get('/', function(req, res, next) {
+  res.send("This is the one");
+});
+
+module.exports = router;
