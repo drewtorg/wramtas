361374e2b6793ca06f272e7ee9e24620ffb3d9f5
Drew Torgeson <drew.torgeson@gmail.com>
2016-09-27 20:29:47 -0600

    Fix university and position recognition

--- public/pages/Application/applicationController.js
+++ public/pages/Application/applicationController.js
@@ -5,31 +5,46 @@ app.controller('applicationController', function($scope, $routeParams, applicati
   $scope.showNotice = false;
   $scope.submitNotice = 'Thank for for submitting your application! The WRAMTAS Executive Board will review all applications on March 13th, 2017 to make sure it contains accurate and appropriate information.  Come back to http://wramtas.org/ after that date to see all the accepted applications.';
   $scope.saveNotice = 'You have saved your application, but it is not yet submitted.  When you are ready, submit your application for final review by hitting the "Submit Application" button.';
+  $scope.submittedNotice = 'Your application has already been submitted.  You will be notified if your application needs to be modified and re-submitted.';
   $scope.errorNotice = 'Your application could not be found.  Please check that you have used the correct link.  If this doesn\'t work, nominate yourself and try the new application sent to you.';
   $scope.application = {};
   applicationService.getApplication($routeParams._id).then(function(response) {
-    if (response.data && !response.data.submitted)
-      $scope.application = response.data;
-    else
+    if (!response.data)
       $scope.popupNotice($scope.errorNotice);
-  })
+    else if (response.data.submitted)
+      $scope.popupNotice($scope.submittedNotice);
+    else {
+      $scope.application = response.data;
+
+      universitiesService.getUniversities().then(function(response) {
+        $scope.universities = response.data;
+        for (var i = 0; i < $scope.universities.length; i += 1) {
+          $scope.universities[i].id = i + 1;
+          if ($scope.application.university === $scope.universities[i]._id) {
+            $scope.application.university = $scope.universities[i];
+          }
+        }
+        if (!$scope.form.university)
+          $scope.form.university = $scope.universities[0];
+      });
+
+      positionsService.getPositions().then(function(response) {
+        $scope.positions = response.data;
+        for (var i = 0; i < $scope.positions.length; i += 1) {
+          $scope.positions[i].id = i + 1;
+          if ($scope.application.position === $scope.positions[i]._id) {
+            $scope.application.position = $scope.positions[i];
+          }
+        }
+        if (!$scope.form.position)
+          $scope.form.position = $scope.positions[0];
+      });
+    }
+  });
 
   // TODO: Check if this is a valid application.  If not, either redirect to the
   // home page or show an error message stating that this application doesn't exist.
 
-  universitiesService.getUniversities().then(function(response) {
-    $scope.universities = response.data;
-    for (var i = 1; i <= $scope.universities.length; i += 1)
-      $scope.universities[i - 1].id = i;
-    $scope.form.university = $scope.universities[0];
-  });
-
-  positionsService.getPositions().then(function(response) {
-    $scope.positions = response.data;
-    for (var i = 1; i <= $scope.positions.length; i += 1)
-      $scope.positions[i - 1].id = i;
-    $scope.form.position = $scope.positions[0];
-  });
 
   $scope.popupNotice = function(notice) {
     $scope.showNotice = true;
