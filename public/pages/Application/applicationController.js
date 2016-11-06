e1673ef4e11912d26842661049224019f4329386
Drew Torgeson <drew.torgeson@gmail.com>
2016-11-06 11:36:41 -0700

    Use tinymce editor for the application bio

--- public/pages/Application/applicationController.js
+++ public/pages/Application/applicationController.js
@@ -9,6 +9,43 @@ app.controller('applicationController', function($scope, $routeParams, applicati
   $scope.errorNotice = 'Your application could not be found.  Please check that you have used the correct link.  If this doesn\'t work, nominate yourself and try the new application sent to you.';
   $scope.application = {};
   $scope.validApplication = false;
+  $scope.tinymceOptions = {
+    selector: 'textarea',
+    theme: 'modern',
+    plugins: [
+      'advlist autolink link image imagetools lists charmap preview hr anchor pagebreak spellchecker',
+      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+      'save table contextmenu directionality template paste textcolor'
+    ],
+    height: 400,
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media fullpage | forecolor',
+    imagetools_toolbar: 'rotateleft rotateright | flipv fliph | editimage imageoptions',
+    style_formats_merge: true,
+    style_formats: [{
+      title: 'Images',
+      items: [{
+        title: 'Float Left',
+        selector: 'img',
+        styles: {
+          'float': 'left',
+          'margin': '0 10px 0 10px'
+        }
+      }, {
+        title: 'Float Right',
+        selector: 'img',
+        styles: {
+          'float': 'right',
+          'margin': '0 10px 0 10px'
+        }
+      }, {
+        title: 'Shadow Box',
+        selector: 'img',
+        styles: {
+          'box-shadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2)'
+        }
+      }]
+    }]
+  };
   applicationService.getApplication($routeParams._id).then(function(response) {
     if (!response.data)
       $scope.popupNotice($scope.errorNotice);
