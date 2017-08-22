bcc1d9098f970aceec1f1e0ac4db25c771530392
Drew Torgeson <drew.torgeson@gmail.com>
2017-08-21 22:36:35 -0700

    Show all nominations to admins on hte nominations page

--- public/pages/Nominate/nominateController.js
+++ public/pages/Nominate/nominateController.js
@@ -1,5 +1,7 @@
 ﻿app.controller('nominateController', function(
     $scope,
+    applicationService,
+    authService,
     electionService,
     nominateService,
     universitiesService,
@@ -7,6 +9,7 @@
   $scope.form = {};
   $scope.universities = {};
   $scope.positions = {};
+  $scope.positionTitles = {};
   $scope.showNotice = false;
   $scope.notice = 'Thank you for submitting your nomination.  An email has been sent to the address you listed.';
   $scope.isElectionRunning = false;
@@ -20,9 +23,16 @@
 
   positionsService.getPositions().then(function(response) {
     $scope.positions = response.data;
-    for (var i = 1; i <= $scope.positions.length; i += 1)
+    for (var i = 1; i <= $scope.positions.length; i += 1) {
       $scope.positions[i - 1].id = i;
+      var id = response.data[i - 1]._id;
+      $scope.positionTitles[id] = response.data[i - 1].title;
+    }
     $scope.form.position = $scope.positions[0];
+  });
+
+  applicationService.getApplications().then(function(response) {
+    $scope.candidates = response.data;
   });
 
   electionService.getCurrentElection().then(function(election) {
@@ -36,5 +46,18 @@
   $scope.submitNomination = function(form) {
     $scope.showNotice = true;
     nominateService.sendNominationEmail(form);
+  };
+
+  $scope.isAdmin = function() {
+    return authService.isAdmin();
+  };
+
+  $scope.hasCandidates = function(candidateGroup) {
+    for (var index in candidateGroup) {
+      if (candidateGroup[index]) {
+        return true;
+      }
+    }
+    return false;
   };
 });
