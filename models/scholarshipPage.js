f94a8d34af59fac04ffb22a857281e0faf8e5ca8
Drew Torgeson <drew.torgeson@gmail.com>
2017-08-21 21:53:30 -0700

    Replace the scholarships directives with the submissions directives

--- models/scholarshipPage.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var ScholarshipPage = new Schema({
-  html: String
-});
-
-module.exports = mongoose.model('ScholarshipPage', ScholarshipPage);
