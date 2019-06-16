5da6f07fd86274926fc09d20fbbddb2748e533ac
Drew Torgeson <drew.torgeson@gmail.com>
2019-06-16 14:41:44 -0700

    Using typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,42 @@
-let contentfulConfig
+let contentfulConfig;
 
 try {
   // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
+  contentfulConfig = require("./.contentful");
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
+    "Contentful spaceId and the delivery token need to be provided."
+  );
 }
 
 module.exports = {
-  pathPrefix: '/gatsby-contentful-starter',
+  siteMetadata: {
+    siteName: `WRAMTAS`
+  },
+  pathPrefix: "/wramtas",
   plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
+    `gatsby-plugin-typescript`,
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+        omitGoogleFont: true
+      }
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig
     }
-  ],
-}
+  ]
+};
