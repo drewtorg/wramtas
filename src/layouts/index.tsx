c4051831e43e22ed5c29790c9921c4faea08b501
Drew Torgeson <drew.torgeson@gmail.com>
2019-06-16 23:03:54 -0700

    Import bootstrap only on the client side of the build

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import '../scss/main.scss';
 import Header from '../components/Header';
-import 'bootstrap/dist/js/bootstrap.js';
 
 const MainLayout: React.SFC = ({ children }) => (
   <div>
