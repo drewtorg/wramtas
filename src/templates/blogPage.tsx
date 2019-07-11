7a0a725f7dc353bc48f74e89d241a69d794b2a95
Drew Torgeson <drew.torgeson@gmail.com>
2019-07-11 09:14:56 -0700

    Sort blog posts by publish date

--- src/templates/blogPage.tsx
+++ src/templates/blogPage.tsx
@@ -3,24 +3,28 @@ import { graphql, Link } from 'gatsby';
 import MainLayout from '../layouts';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
+const renderPosts = (posts: any): any => {
+  return []
+    .concat(posts)
+    .sort((a: any, b: any) => (new Date(a.publishDate) < new Date(b.publishDate) ? 1 : -1))
+    .map((post: any) => (
+      <div key={post.id} className="row">
+        <div className="col">
+          <Link to={post.slug} className="blog-title">
+            {post.title}
+          </Link>
+          {documentToReactComponents(post.bodyHtml.json)}
+          <p>Published: {new Date(post.publishDate).toDateString()}</p>
+        </div>
+      </div>
+    ));
+};
+
 const BlogPage = ({ data }: any) => {
   const posts = data.contentfulBlogPage.blogPosts;
   return (
     <MainLayout>
-      <div className="blogpage">
-        {posts !== null &&
-          posts.map((post: any) => (
-            <div key={post.id} className="row">
-              <div className="col">
-                <Link to={post.slug} className="blog-title">
-                  {post.title}
-                </Link>
-                {documentToReactComponents(post.bodyHtml.json)}
-                <p>Published: {new Date(post.publishDate).toDateString()}</p>
-              </div>
-            </div>
-          ))}
-      </div>
+      <div className="blogpage">{posts !== null && renderPosts(posts)}</div>
     </MainLayout>
   );
 };
