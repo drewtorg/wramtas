3d0847a95953b4160d874e17e27edffdeae1cb2c
Drew Torgeson <drew.torgeson@gmail.com>
2019-12-04 18:33:47 -0800

    try to redirect when http

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,11 @@
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+export function onPreRouteUpdate({location}) {
+    console.log("Routing to location: ", location);
+
+    if (location.origin === 'http://www.wramtas.org') {
+        window.location.replace(location.href.replace('http', 'https'));
+    }
+}
