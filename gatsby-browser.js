6d221c0c59683b3c756891500e7d55993ad5ee30
Drew Torgeson <drew.torgeson@gmail.com>
2019-12-04 18:41:31 -0800

    Add redirection to staging too

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,7 @@ import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 export function onPreRouteUpdate({location}) {
-    console.log("Routing to location: ", location);
-
-    if (location.origin === 'http://www.wramtas.org') {
+    if (location.origin === 'http://www.wramtas.org' || location.origin === 'http://wramtas-staging.herokuapp.com') {
         window.location.replace(location.href.replace('http', 'https'));
     }
 }
